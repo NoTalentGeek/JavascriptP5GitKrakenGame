e9080e1e3ac0d07a5d356a166f6bf664df296c66
notalentgeek <mikael.pratama@yahoo.com>
2016-04-16 17:12:58 +0200

    Trail entrance animation based on container object.
    However the trail is not following the container yet.

--- JavascriptP5PointAndTrailManager/sketch.js
+++ JavascriptP5PointAndTrailManager/sketch.js
@@ -1,7 +1,22 @@
+var test_Container_Object;
+var testCounter_Int = 0;
+
+
+
+
+
 /*==================================================*/
 function setup() {
 
     createCanvas(960,540);
+
+
+
+
+
+    test_Container_Object = new Container_Object(global_offset_Int, global_offset_Int, width - global_offset_Int*2, height - global_offset_Int*2);
+    var test_Trail_Object = new Trail_Object("1", 0 + global_offset_Int, height - global_offset_Int, false, test_Container_Object);
+    test_Container_Object.AddComponent_Container_Object(test_Trail_Object);
 
 }
 /*==================================================*/
@@ -14,6 +29,37 @@ function setup() {
 function draw() {
 
     background(global_backgroundColor_String);
+
+
+    testCounter_Int ++;
+    if(testCounter_Int%100 == 0){
+
+        var test_Trail_Object = new Trail_Object("1", 0 + global_offset_Int, height - global_offset_Int, false, test_Container_Object);
+        test_Container_Object.AddComponent_Container_Object(test_Trail_Object);
+
+    }
+
+
+    test_Container_Object.Draw_Container_Object();
+    //test_Container_Object.x_Int ++;
+    //console.log(test_Container_Object.insideComponent_Dynamic_Array.length);
+    //if(test_Container_Object.insideComponent_Dynamic_Array[0] != null){ test_Container_Object.insideComponent_Dynamic_Array[0].Set_staticAxis_Int(test_Container_Object.insideComponent_Dynamic_Array[0].staticAxis_Int + 1); }
+    /*
+    for(
+        var i_Int = 0;
+        i_Int < test_Container_Object.insideComponent_Dynamic_Array.length;
+        i_Int
+    ){
+
+        test_Container_Object
+            .insideComponent_Dynamic_Array[i_Int]
+            .DetermineDestinedAxis_Trail_Object(test_Container_Object.width);
+
+        console.log(test_Container_Object.insideComponent_Dynamic_Array[i_Int].staticAxis_Int + 1);
+
+    }
+    */
+    Update_global_Trail_Object_Array_Void();
 
 
 
--- JavascriptP5PointAndTrailManager/src/object/Trail_Object.js
+++ JavascriptP5PointAndTrailManager/src/object/Trail_Object.js
@@ -1,28 +1,49 @@
 /*==================================================*/
 var Trail_Object = function(
     _name_String,
+    _endAxis_Int,
+    _startAxis_Int,
     _horizontal_Bool,
     __Container_Object
 ){
 
     this._Container_Object      = __Container_Object;
+    this.endAxis_Int            = _endAxis_Int;
     this.horizontal_Bool        = _horizontal_Bool;
     this.name_String            = _name_String;
+    this.startAxis_Int          = _startAxis_Int;
 
 
 
 
 
-    this.endAxis_Int;
+    this.destinedAxis_Int;
     this.index_Int;
-    this.startAxis_Int;
+    this.indexTrue_Int;
     this.staticAxis_Int;
     this.animation_Bool         = true;
+    this.animation_Fix          = false;
     this.animationIn_Bool       = true;
     this.animationOut_Bool      = false;
     this.prepareDelete_Bool     = false;
     this.prepareSetup_Bool      = true;
     this.ready_Bool             = false;
+
+
+
+
+
+    if(this.horizontal_Bool == true){ this.staticAxis_Int = this.Set_staticAxis_Int(this._Container_Object.height_Int + global_offset_Int); }
+    else if(this.horizontal_Bool == false){ this.staticAxis_Int = this.Set_staticAxis_Int(this._Container_Object.width_Int + global_offset_Int); }
+    else{ this.staticAxis_Int = this.Set_staticAxis_Int(this._Container_Object.height_Int + global_offset_Int); }
+
+
+
+
+
+    //console.log(global_offset_Int);
+    //console.log(this.staticAxis_Int);
+
 
 
 
@@ -40,8 +61,90 @@ Trail_Object.prototype.constructor = Trail_Object;
 /*==================================================*/
 Trail_Object.prototype.AddToArray_Trail_Object = function(){
 
-    global_Trail_Object_Array.add(this);
+    global_Trail_Object_Array.push(this);
     this.index_Int = global_Trail_Object_Array.indexOf(this);
+
+
+
+    for(
+        var i_Int = 0;
+        i_Int < global_Trail_Object_Array.length;
+        i_Int ++
+    ){
+
+        //console.log(global_Trail_Object_Array[i_Int]);
+
+
+
+
+
+        if(
+            global_Trail_Object_Array[i_Int] != null &&
+            global_Trail_Object_Array[i_Int] !== undefined
+        ){ global_Trail_Object_Array[i_Int].DetermineTrueIndex_Trail_Object(); }
+
+    }
+
+
+
+
+
+    //console.log(this.indexTrue_Int);
+
+
+
+
+
+    for(
+        var i_Int = 0;
+        i_Int < global_Trail_Object_Array.length;
+        i_Int ++
+    ){
+
+        if(
+            global_Trail_Object_Array[i_Int] != null &&
+            global_Trail_Object_Array[i_Int] !== undefined
+        ){
+
+            var maxLength_Int;
+            if(
+                global_Trail_Object_Array[i_Int]._Container_Object_Object != null &&
+                global_Trail_Object_Array[i_Int]._Container_Object_Object !== undefined
+            ){
+
+                if(global_Trail_Object_Array[i_Int].horizontal_Bool == true)
+                    { maxLength_Int = _Container_Object.height; }
+                else if(global_Trail_Object_Array[i_Int].horizontal_Bool == false)
+                    { maxLength_Int = _Container_Object.width; }
+
+            }
+            else if(
+                global_Trail_Object_Array[i_Int]._Container_Object_Object == null ||
+                global_Trail_Object_Array[i_Int]._Container_Object_Object === undefined
+            ){
+
+                if(global_Trail_Object_Array[i_Int].horizontal_Bool == true)
+                    { maxLength_Int = height; }
+                else if(global_Trail_Object_Array[i_Int].horizontal_Bool == false)
+                    { maxLength_Int = width; }
+
+            }
+
+
+
+
+            console.log(maxLength_Int);
+            global_Trail_Object_Array[i_Int].DetermineDestinedAxis_Trail_Object(maxLength_Int);
+
+        }
+
+    }
+
+
+
+
+
+    console.log(this.destinedAxis_Int);
 
 
 
@@ -73,8 +176,7 @@ Trail_Object.prototype.AnimationControl_Trail_Object = function(){
 
     if(this.prepareDelete_Bool == true){
 
-        //this.AnimationOut_Trail_Object();
-        this.animationOut_Bool = true;
+        this.AnimationOut_Trail_Object();
 
     }
     if(
@@ -93,8 +195,7 @@ Trail_Object.prototype.AnimationControl_Trail_Object = function(){
 
     if(this.prepareSetup_Bool == true){
 
-        //this.AnimationIn_Trail_Object();
-        this.animationIn_Bool = true;
+        this.AnimationIn_Trail_Object();
 
     }
     if(
@@ -111,6 +212,19 @@ Trail_Object.prototype.AnimationControl_Trail_Object = function(){
 
 
 
+    if(
+        this.destinedAxis_Int != this.staticAxis_Int &&
+        this.ready_Bool == true
+    ){
+
+        AnimationFix_Trail_Object();
+
+    }
+
+
+
+
+
     return this;
 
 };
@@ -121,7 +235,51 @@ Trail_Object.prototype.AnimationControl_Trail_Object = function(){
 
 
 /*==================================================*/
+Trail_Object.prototype.AnimationFix_Trail_Object = function(){
+
+        this.animation_Bool = true;
+
+
+
+
+
+        if(this.destinedAxis_Int < this.staticAxis_Int)
+            { this.staticAxis_Int = Set_staticAxis_Int(this.staticAxis_Int - global_animationSpeed_Int); }
+        else if(this.destinedAxis_Int > this.staticAxis_Int)
+            { this.staticAxis_Int = Set_staticAxis_Int(this.staticAxis_Int + global_animationSpeed_Int); }
+        else if(this.destinedAxis_Int == this.staticAxis_Int)
+            { this.animation_Bool = false; }
+
+}
+/*==================================================*/
+
+
+
+
+
+/*==================================================*/
 Trail_Object.prototype.AnimationIn_Trail_Object = function(){
+
+    this.animation_Bool = true;
+
+
+
+
+
+    if(this.destinedAxis_Int < this.staticAxis_Int)
+        { this.staticAxis_Int = this.Set_staticAxis_Int(this.staticAxis_Int - global_animationSpeed_Int); }
+    else if(this.destinedAxis_Int > this.staticAxis_Int){
+
+        this.staticAxis_Int = this.Set_staticAxis_Int(this.destinedAxis_Int);
+        this.animation_Bool = false;
+
+    }
+    else if(this.destinedAxis_Int == this.staticAxis_Int){ this.animation_Bool = false; }
+
+
+
+
+
 
     return this;
 
@@ -135,7 +293,102 @@ Trail_Object.prototype.AnimationIn_Trail_Object = function(){
 /*==================================================*/
 Trail_Object.prototype.AnimationOut_Trail_Object = function(){
 
+    this.animation_Bool = true;
+
+
+
+
+
+    if(this.horizontal_Bool == true){
+
+        if(width + global_offset_Int != this.staticAxis_Int)
+            { this.staticAxis_Int = Set_staticAxis_Int(this.staticAxis_Int + global_animationSpeed_Int); }
+        if(width + global_offset_Int == this.staticAxis_Int){ this.animation_Bool = false; }
+
+    }
+    else if(this.horizontal_Bool == false){
+
+        if(height + global_offset_Int != this.staticAxis_Int)
+            { this.staticAxis_Int = Set_staticAxis_Int(this.staticAxis_Int + global_animationSpeed_Int); }
+        if(height + global_offset_Int == this.staticAxis_Int){ this.animation_Bool = false; }
+
+    }
+    else{
+
+        if(width + global_offset_Int != this.staticAxis_Int)
+            { this.staticAxis_Int = Set_staticAxis_Int(this.staticAxis_Int + global_animationSpeed_Int); }
+        if(width + global_offset_Int == this.staticAxis_Int){ this.animation_Bool = false; }
+
+    }
+
+
+
+
+
     return this;
+
+};
+/*==================================================*/
+
+
+
+
+
+/*==================================================*/
+Trail_Object.prototype.DetermineDestinedAxis_Trail_Object = function(_maxLength_Int){
+
+    var trailInterval_Int = _maxLength_Int/(CheckTrueArrayLength_Int(global_Trail_Object_Array) + 1);
+
+
+
+
+
+    console.log(_maxLength_Int);
+    console.log(CheckTrueArrayLength_Int(global_Trail_Object_Array) + 1);
+    console.log(trailInterval_Int);
+
+
+
+
+
+    this.destinedAxis_Int = (this.indexTrue_Int + 1)*trailInterval_Int;
+
+
+
+
+
+    console.log(this.destinedAxis_Int);
+
+}
+/*==================================================*/
+
+
+
+
+
+/*==================================================*/
+Trail_Object.prototype.DetermineTrueIndex_Trail_Object = function(){
+
+    //console.log("Hello world!");
+
+
+
+
+
+    var decreaseCounter_Int = 0;
+    for(
+        var i_Int = 0;
+        i_Int < this.index_Int;
+        i_Int ++
+    ){
+
+        if(
+            global_Trail_Object_Array[i_Int] == null ||
+            global_Trail_Object_Array[i_Int] === undefined
+        ){ decreaseCounter_Int ++; }
+
+    }
+    this.indexTrue_Int = this.index_Int - decreaseCounter_Int;
 
 };
 /*==================================================*/
@@ -164,6 +417,12 @@ Trail_Object.prototype.Draw_Trail_Object = function(){
 
     }
     else if(this.horizontal_Bool == false){
+
+        //rect(520, height/2 - 50, 100, 100);
+        //console.log(this.staticAxis_Int);
+
+
+
 
         line(
             this.staticAxis_Int,
@@ -280,19 +539,7 @@ Trail_Object.prototype.Set_staticAxis_Int = function(_staticAxis_Int){
 
 
 /*==================================================*/
-Trail_Object.prototype.Update_Trail_Object = function(
-    _staticAxis_Int,
-    _endAxis_Int,
-    _startAxis_Int
-){
-
-    this.endAxis_Int        = this.Set_endAxis_Int(_endAxis_Int);
-    this.startAxis_Int      = this.Set_startAxis_Int(_startAxis_Int);
-    this.staticAxis_Int     = this.Set_staticAxis_Int(_staticAxis_Int);
-
-
-
-
+Trail_Object.prototype.Update_Trail_Object = function(){
 
     this.AnimationControl_Trail_Object();
     this.Draw_Trail_Object(
